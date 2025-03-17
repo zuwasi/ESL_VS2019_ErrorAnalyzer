f389868ca98125c002d7ae9943a00bd4300fcea8
zuwasi <liezrowice@yahoo.com>
2025-03-17 15:13:43 +0200

    Update run_analysis.bat

--- run_analysis.bat
+++ run_analysis.bat
@@ -1,3 +1,5 @@
+
+REM This is an example of how do I take the analyzer report from a sample project called 4_6_1 and generate errors.log file that will be procced by the ESL_VS2019_ErrorAnalyzer.exe
 @echo off
 echo Running MSBuild...
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" C:\Users\danie\source\repos\4_6_1\4_6_1.sln /t:Rebuild /v:diag /fl /flp:logfile=C:\Users\danie\source\repos\4_6_1\errors.log
